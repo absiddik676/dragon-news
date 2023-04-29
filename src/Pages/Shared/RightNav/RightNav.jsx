586d4f457b8cd60f27af229229dc499b91ce4854
absiddik676 <absiddik8200@gmail.com>
2023-04-29 23:38:38 +0600

    header done and right nav working

--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
+import { FaGoogle,FaGithub } from 'react-icons/fa';
 const RightNav = () => {
     return (
         <div>
-            <h2>Right  nav</h2>
+            <h4>Login With</h4>
+            <Button className='mb-2 px-3' variant="outline-primary">Login with Google <FaGoogle/></Button>
+            <Button variant="outline-secondary">Login with Github <FaGithub/></Button>
+            <div>
+                <h4></h4>
+            </div>
         </div>
     );
 };
